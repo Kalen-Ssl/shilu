05629069e41303dcc90044e4d08e37eae6618b9c
Karen <2071004049@qq.com>
2020-01-12 19:04:51 +0800

    LeetCode189

---
+++ Project1_12/LeetCode189.java
@@ -0,0 +1,13 @@
+
+public class LeetCode189 {
+    void rotate(int[] nums, int k) {
+        k = k % nums.length;
+        for (int i = 0; i < k; i++) {
+            int fir = nums[nums.length - 1];
+            for (int j = nums.length - 2; j >= 0; j--) {
+                nums[j + 1] = nums[j];
+            }
+            nums[0] = fir;
+        }
+    }
+}
