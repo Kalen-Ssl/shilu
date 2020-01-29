f4a0a81055e89a59f66a27667cc3a7856281c698
Karen <2071004049@qq.com>
2020-01-29 21:35:31 +0800

    提交代码

---
+++ Project1_29/LeetCode922.java
@@ -0,0 +1,17 @@
+
+public class LeetCode922 {
+        public int[] sortArrayByParityII(int[] A) {
+            int j = 1;
+            for (int i = 0; i < A.length - 1; i = i + 2) {
+                if ((A[i] & 1) != 0) {
+                    while ((A[j] & 1) != 0) {
+                        j = j + 2;
+                    }
+                    int tmp = A[i];
+                    A[i] = A[j];
+                    A[j] = tmp;
+                }
+            }
+            return A;
+        }
+}
