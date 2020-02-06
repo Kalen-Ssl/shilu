9bc37f3475317007412571a5eae5a2ea702b055d
Karen <2071004049@qq.com>
2020-02-06 19:45:22 +0800

    力扣

---
+++ Project2_6/LeetCode268.java
@@ -0,0 +1,11 @@
+import java.util.Arrays;
+
+public class LeetCode268 {
+    public int missingNumber(int[] nums) {
+        Arrays.sort(nums);
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != i) return i;
+        }
+        return nums.length;
+    }
+}
