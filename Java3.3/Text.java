0e30da71cb18935b272fe057a51042d07da74846
Karen <2071004049@qq.com>
2020-03-03 21:37:12 +0800

    上传代码

---
+++ Java3.3/Text.java
@@ -0,0 +1,19 @@
+
+
+    public class Text{
+        String str = new String("good");
+        char[ ] ch = { 'a' , 'b' , 'c' };
+        public static void main(String args[]){
+            Text ex = new Text();
+            ex.change(ex.str,ex.ch);
+            System.out.print(ex.str + " and ");
+            System.out.print(ex.ch);
+        }
+        public void change(String str,char ch[ ]){
+            str = "test ok";
+            ch[0] = 'g';
+        }
+
+
+    }
+
