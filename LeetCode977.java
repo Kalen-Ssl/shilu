c9b5b87ce2776c8a7fdb071f822382069195621f
Karen <2071004049@qq.com>
2020-03-19 20:00:41 +0800

    代码

---
+++ LeetCode977.java
@@ -0,0 +1,20 @@
+package LeetCode;
+
+class LeetCode977 {
+    public int[] sortedSquares(int[] A) {
+        for(int i=0;i<A.length;i++){
+            A[i]=A[i]*A[i];
+        }
+        int t;
+        for(int j=0;j<A.length-1;j++){
+            for(int m=j+1;m<A.length;m++){
+                if(A[j]>A[m]){
+                    t=A[j];
+                    A[j]=A[m];
+                    A[m]=t;
+                }
+            }
+        }
+        return A;
+    }
+}
