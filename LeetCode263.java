fc385c8ce2f1e39e4d121ec9e0e964b50118ef54
Karen <2071004049@qq.com>
2020-04-17 22:48:43 +0800

    代码

---
+++ LeetCode263.java
@@ -0,0 +1,16 @@
+
+public class LeetCode263{
+    public boolean isUgly(int num) {
+        if (num<1) return false;
+        while (num%5==0){
+            num/=5;
+        }
+        while (num%3==0){
+            num/=3;
+        }
+        while (num%2==0){
+            num>>=1;
+        }
+        return num == 1;
+    }
+}
