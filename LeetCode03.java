3eb7a9b03a92589f88695b6e99b49ea41b9a40fb
Karen <2071004049@qq.com>
2020-05-05 22:31:59 +0800

    代码

---
+++ LeetCode03.java
@@ -0,0 +1,54 @@
+package LeetCode;
+
+//两个栈实现一个队列
+import java.util.Stack;
+
+class MyQueue {
+    private Stack<Integer> stack1;
+    private Stack<Integer> stack2;
+
+    /** Initialize your data structure here. */
+    public MyQueue() {
+        stack1 = new Stack<>();
+        stack2 = new Stack<>();
+    }
+
+    /** Push element x to the back of queue. */
+    public void push(int x) {
+        stack1.push(x);
+    }
+
+    /** Removes the element from in front of queue and returns that element. */
+    public int pop() {
+        while (!stack1.isEmpty()) {
+            stack2.push(stack1.pop());
+        }
+        int res = stack2.pop();
+        while (!stack2.isEmpty()) {
+            stack1.push(stack2.pop());
+        }
+        return res;
+    }
+
+    /** Get the front element. */
+    public int peek() {
+        while (!stack1.isEmpty()) {
+            stack2.push(stack1.pop());
+        }
+        int res = stack2.peek();
+        while (!stack2.isEmpty()) {
+            stack1.push(stack2.pop());
+        }
+        return res;
+    }
+
+    /** Returns whether the queue is empty. */
+    public boolean empty() {
+        return stack1.isEmpty();
+    }
+}
+
+
+public class LeetCode03 {
+
+}
